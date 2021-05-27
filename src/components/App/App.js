1f2b0f324ea5da3414f80030fb4b3b35309ae36f
EvilRedHorse <39168275+EvilRedHorse@users.noreply.github.com>
2021-05-26 23:13:29 -0400

    Update App.js
    disabling all but HomeUpload
    used as a simple embedded widget

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,28 +1,36 @@
 import React from "react";
-import TopSwoosh from "../../svg/TopSwoosh";
+//import TopSwoosh from "../../svg/TopSwoosh";
 import "./App.scss";
-import { HomeTop, HomeUpload, HomeBuilt, HomeSamples, HomeStay, HomeNetwork, Footer } from "../";
-
+//import { HomeTop, HomeUpload, HomeBuilt, HomeSamples, HomeStay, HomeNetwork, Footer } from "../";
+import { HomeUpload } from "../";
 function App() {
   return (
     <div className="app">
+/*
       <TopSwoosh className="top-swoosh" />
-
+*/
       <div className="width">
+/*
         <HomeTop />
+*/
         <HomeUpload />
+/*
         <HomeBuilt />
         <HomeSamples />
+*/
       </div>
 
       <div className="home-white">
         <div className="width">
+/*
           <HomeStay />
           <HomeNetwork />
+*/
         </div>
       </div>
-
+/*
       <Footer />
+/*
     </div>
   );
 }
